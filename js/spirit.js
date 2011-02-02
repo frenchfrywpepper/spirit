d8fd6e250fca0245b57263773bf17d226e310f38
Sarah Haskins <sarah@frenchfrieswithpepper.com>
2011-02-01 21:52:38 -0500

    working non-clustered version

--- js/spirit.js
+++ js/spirit.js
@@ -1,7 +1,6 @@
 (function() {
-  window.onload = function() {
-
-   		var infoDiv = document.getElementById('info');
+	window.onload = function() {
+		var infoDiv = document.getElementById('info');
       	var zoomInLink = document.getElementById('zoomInLink');   	
 		var siteNameElem = document.getElementById('siteName');
 		var streetElem = document.getElementById('street');
@@ -9,105 +8,88 @@
 		var licenseeElem = document.getElementById('licensee');
 		var siteNumElem = document.getElementById('siteNum');
   
-	// Creating a reference to the mapDiv
-    var mapDiv = document.getElementById('map');
+		var mapDiv = document.getElementById('map');
+		
+		// guesstimate for center of map
+    	var latlng = new google.maps.LatLng(37.09, -95.71);
     
-    // Creating a latLng for the center of the map
-    var latlng = new google.maps.LatLng(37.09, -95.71);
-    
-    // Creating an object literal containing the properties 
-    // we want to pass to the map  
-    var options = {
-      center: latlng,
-      zoom: 4,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    
-    // Creating the map
-    var map = new google.maps.Map(mapDiv, options);
-    
-    // Creating a LatLngBounds object
-    var bounds = new google.maps.LatLngBounds();
-    
-    var infowindow;
-    
-    var doZoomIn = function() {
-        	map.setCenter(marker.getPosition());
-            map.setZoom(15);
-            zoomInLink.innerHTML = 'view all stations';
-            zoomInLink.onclick = doZoomOut;
-            return false;
+    	var options = {
+      		center: latlng,
+      		zoom: 4, // guesstimate for initial zoom level
+      		mapTypeId: google.maps.MapTypeId.ROADMAP
     	};
+    
+    	var map = new google.maps.Map(mapDiv, options);
+    
+    	// track markers added to map for later extending bounds
+    	var bounds = new google.maps.LatLngBounds();
+    
+    	var infowindow;
     	
-      	var doZoomOut = function() {
-        	map.setCenter(marker.getPosition());
-            map.fitBounds(bounds);
-            zoomInLink.innerHTML = 'zoom to station';
-            zoomInLink.onclick = doZoomIn;
-            return false;
-    	};
-    var addStations = function(idx, value) {
-   		var lng = value.geometry.coordinates[0];
-   		var lat = value.geometry.coordinates[1];
-   		var name = value.properties.siteName;
-   		var formattedAddress = value.properties.formattedAddress;
-   		var address = value.properties.address;
-   		var city = value.properties.city;
-   		var state = value.properties.state;
-   		var zip = value.properties.zip;
-   		var licensee = value.properties.licensee;
-   		var siteNum = value.properties.num;
+    	var addStations = function(idx, value) {
+   			var lng = value.geometry.coordinates[0];
+   			var lat = value.geometry.coordinates[1];
+   			var name = value.properties.siteName;
+   			var formattedAddress = value.properties.formattedAddress;
+   			var address = value.properties.address;
+   			var city = value.properties.city;
+   			var state = value.properties.state;
+   			var zip = value.properties.zip;
+   			var licensee = value.properties.licensee;
+   			var siteNum = value.properties.num;
+   			var latLng = new google.maps.LatLng(lat, lng);
    		
-   		var latLng = new google.maps.LatLng(lat, lng);
+   			var doZoomIn = function() {
+				map.setCenter(marker.getPosition());
+            	map.setZoom(15);
+            	zoomInLink.innerHTML = 'view all stations';
+            	zoomInLink.onclick = doZoomOut;
+            	google.maps.event.trigger(marker, 'click');
+            	return false;
+    		};
+    	
+      		var doZoomOut = function() {
+        		map.setCenter(marker.getPosition());
+            	map.fitBounds(bounds);
+            	zoomInLink.innerHTML = 'zoom to station';
+            	zoomInLink.onclick = doZoomIn;
+            	return false;
+    		};
+   			
+   			var marker = new google.maps.Marker({
+   				position: latLng,
+   				map: map,
+   				title: name,
+   				icon: 'img/spiritmarkerround.png'
+   			});
    		
-   		var marker = new google.maps.Marker({
-   			position: latLng,
-   			map: map,
-   			title: name,
-   			icon: 'img/spiritmarkerround.png'
-   		});
-   		
-   		// Extending the bounds object with each LatLng
-        bounds.extend(latLng);
+   			bounds.extend(latLng);      	
 
-      	
-
-    	zoomInLink.onclick = doZoomIn;
-   		
-   		// Wrapping the event listener inside an anonymous function 
-      // that we immediately invoke and passes the variable i to.
-      (function(infoDiv, marker) {
-
-        // Creating the event listener. It now has access to the values of
-        // i and marker as they were during its creation
-        google.maps.event.addListener(marker, 'click', function() {
+    		zoomInLink.onclick = doZoomIn;
+      		
+      		(function(infoDiv, marker) {
+        		google.maps.event.addListener(marker, 'click', function() {
           
-          if (!infowindow) {
-            infowindow = new google.maps.InfoWindow();
-          }          
+          			if (!infowindow) {
+            			infowindow = new google.maps.InfoWindow();
+          			}          
           
           			siteNameElem.innerHTML = name;
-   		streetElem.innerHTML = address;
-   		cityStateElem.innerHTML = city + ', ' + state + ' ' + zip;
-   		licenseeElem.innerHTML = licensee;
-   		siteNumElem.innerHTML = siteNum;	
+   					streetElem.innerHTML = address;
+   					cityStateElem.innerHTML = city + ', ' + state + ' ' + zip;
+   					licenseeElem.innerHTML = licensee;
+   					siteNumElem.innerHTML = siteNum;	
           
-          // Setting the content of the InfoWindow
-          infowindow.setContent(infoDiv);
-
-          // Tying the InfoWindow to the marker 
-          infowindow.open(map, marker);
-          infoDiv.style.visibility = 'visible';
-        });
-
-      })(infoDiv, marker);
-   	}
+                	infowindow.setContent(infoDiv);
+          			infowindow.open(map, marker);
+          			infoDiv.style.visibility = 'visible';
+        		});
+			})(infoDiv, marker);
+		}
    	
-    $.get("data/spirit.geojson", function(myJSONtext) {
-   		var stations = eval('(' + myJSONtext + ')');
-   		jQuery.each(stations.features, addStations);
-   	});
-   	
-	map.fitBounds(bounds);
-  }
+    	$.getJSON('data/spirit.geojson', function(json) {
+   			jQuery.each(json.features, addStations);
+   			map.fitBounds(bounds);
+   		});
+	}
 })();
